5fe6dde2920bf3e349e7e08f5eb2c56b2e23b59e
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-06-05 09:40:10 +0200

    Integration fixed

--- tests/integration.rs
+++ tests/integration.rs
@@ -76,7 +76,7 @@ const QUERIES: &[&str] = &[
     t2 AS (SELECT 0.1*x as a, 2*x as b FROM table_2)
     SELECT * FROM t1 UNION SELECT * FROM t2",
     // Some joins
-    "SELECT * FROM primary_table LEFT JOIN secondary_table on id=primary_id WHERE amount>0"
+    "SELECT * FROM order_table LEFT JOIN item_table on id=order_id WHERE price>10"
 ];
 
 #[cfg(feature = "sqlite")]
