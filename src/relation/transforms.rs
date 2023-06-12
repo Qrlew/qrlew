aba75c615162e5d3daa0837200b75bfe89eee462
victoria de sainte agathe <vdsa@sarus.tech>
2023-06-12 12:41:49 +0200

    ok

--- src/relation/transforms.rs
+++ src/relation/transforms.rs
@@ -372,18 +372,28 @@ impl Relation {
         // Multiply by weights
         let mut grouping_cols: Vec<Expr> = vec![];
         let mut weighted_agg: Vec<Expr> = vec![];
-        let length = if base.is_empty() {
-            coordinates.len() + 1
+        let left_len = if base.is_empty() {
+            self.schema().len() + 1
         } else {
-            base.len() + coordinates.len()
+            self.schema().len()
         };
+        let join_len = join.schema().len();
         let out_fields = join.schema().fields();
         let in_fields = join.input_fields();
-        for i in 0..(length + 1) {
+        for i in 0..left_len {
+            // length + 1
             if coordinates.contains(&in_fields[i].name()) {
+                let mut pos = i + 1;
+                while &in_fields[i].name() != &in_fields[pos].name() {
+                    pos += 1;
+                    if pos > join_len {
+                        panic!()
+                    }
+                }
+
                 weighted_agg.push(Expr::multiply(
                     Expr::col(out_fields[i].name()),
-                    Expr::col(out_fields[length + i].name()),
+                    Expr::col(out_fields[pos].name()),
                 ));
             } else {
                 grouping_cols.push(Expr::col(out_fields[i].name()));
@@ -457,11 +467,9 @@ impl Relation {
             vectors_base.extend(base.clone());
             self.sum_by(vectors_base, coordinates.clone())
         };
-        aggregated_relation.display_dot().unwrap();
 
         let weighted_relation =
             aggregated_relation.apply_weights(weights, vectors, base.clone(), coordinates.clone());
-        weighted_relation.display_dot().unwrap();
 
         weighted_relation.sum_by(base, coordinates)
     }
@@ -477,9 +485,9 @@ impl With<(&str, Expr)> for Relation {
 mod tests {
     use super::*;
     use crate::{
+        data_type::value::List,
         display::Dot,
         io::{postgresql, Database},
-        relation::{builder::*, schema::Schema, Table},
         sql::parse,
     };
     use colored::Colorize;
@@ -738,6 +746,32 @@ mod tests {
         }
     }
 
+    fn refacto_results(results: Vec<List>, size: usize) -> Vec<Vec<String>> {
+        let mut sorted_results: Vec<Vec<String>> = vec![];
+        for row in results {
+            let mut str_row = vec![];
+            for i in 0..size {
+                let float_i: Result<f64, _> = row[i].to_string().parse();
+                str_row.push(match float_i {
+                    Ok(f) => ((f * 1000.).round() / 1000.).to_string(),
+                    Err(_) => row[i].to_string(),
+                })
+                // let my_float:f64 = row[i].to_string().parse().unwrap();
+                // str_row.push()
+                // (float_number * 1000.0).round() / 1000.0
+                // str_row.push(
+                //     match my_str.find('.') {
+                //         Some(ind) => my_str[..ind+4].to_string(),
+                //         None => my_str + &".000"
+                //     }
+                // )
+            }
+            sorted_results.push(str_row)
+        }
+        sorted_results.sort();
+        sorted_results
+    }
+
     #[test]
     fn test_clipped_sum_for_table() {
         let mut database = postgresql::test_database();
@@ -764,11 +798,7 @@ mod tests {
         "#;
         let my_res = database.query(query).unwrap();
         let true_res = database.query(valid_query).unwrap();
-        assert_eq!(my_res.len(), true_res.len(),);
-        // for i in 0..6 {
-        //     assert_eq!(my_res[i], true_res[i])
-        // }
-        // TODO: I have checked that manually but we need an automatic test
+        assert_eq!(refacto_results(my_res, 2), refacto_results(true_res, 2));
     }
 
     #[test]
@@ -819,9 +849,28 @@ mod tests {
                 .clone()
                 .clipped_sum("order_id", vec!["item"], vec!["price", "std_price"], 45.);
         clipped_relation.display_dot().unwrap();
+
         let query: &str = &ast::Query::from(&clipped_relation).to_string();
-        database.query(query).unwrap();
-        //TODO: complete that
+        let valid_query = r#"
+        WITH my_table AS (
+            SELECT price * 25 AS std_price, * FROM item_table
+          ), norms AS (
+            SELECT order_id, SQRT(SUM(sum_by_group)) AS norm1, SQRT(SUM(sum_by_group2)) AS norm2 FROM (
+              SELECT order_id, item, POWER(SUM(price), 2) AS sum_by_group, POWER(SUM(std_price), 2) AS sum_by_group2 FROM my_table GROUP BY order_id, item
+            ) AS subquery GROUP BY order_id
+          ), weights AS (SELECT order_id, CASE WHEN 45 / norm1 < 1 THEN 45 / norm1 ELSE 1 END AS weight1, CASE WHEN 45 / norm2 < 1 THEN 45 / norm2 ELSE 1 END AS weight2 FROM norms)
+          SELECT item, SUM(price*weight1), SUM(std_price*weight2) FROM my_table LEFT JOIN weights USING (order_id) GROUP BY item;
+        "#;
+
+        let my_res = refacto_results(database.query(query).unwrap(), 3);
+        let true_res = refacto_results(database.query(valid_query).unwrap(), 3);
+        // for (r1, r2) in my_res.iter().zip(true_res.iter()) {
+        //     if r1 != r2 {
+        //         println!("my_res = {:?}", r1);
+        //         println!("true_res = {:?}", r2);
+        //     }
+        // }
+        assert_eq!(my_res, true_res);
     }
 
     #[test]
@@ -855,8 +904,27 @@ mod tests {
         let date = schema.field_from_index(6).unwrap().name();
 
         // TODO: complete the test
-        // let clipped_relation = relation.clipped_sum(user_id, vec![item, date], vec![price], 50.);
-        // clipped_relation.display_dot().unwrap();
-        // let query: &str = &ast::Query::from(&clipped_relation).to_string();
+        let clipped_relation = relation.clipped_sum(user_id, vec![item, date], vec![price], 50.);
+        clipped_relation.display_dot().unwrap();
+        let query: &str = &ast::Query::from(&clipped_relation).to_string();
+        let valid_query = r#"
+        WITH join_table AS (
+            SELECT * FROM item_table JOIN order_table ON item_table.order_id = order_table.id
+           ), norms AS (
+            SELECT user_id, SQRT(SUM(sum_1)) AS norm FROM (SELECT user_id, item, date, POWER(SUM(price), 2) AS sum_1 FROM join_table  GROUP BY user_id, item, date) As subq GROUP BY user_id
+           ), weights AS (
+             SELECT user_id, CASE WHEN 50 / norm < 1 THEN 50 / norm ELSE 1 END AS weight FROM norms
+           ) SELECT item, date, SUM(price*weight)  FROM join_table LEFT JOIN weights USING (user_id) GROUP BY item, date;
+        "#;
+
+        let my_res = refacto_results(database.query(query).unwrap(), 3);
+        let true_res = refacto_results(database.query(valid_query).unwrap(), 3);
+        // for (r1, r2) in my_res.iter().zip(true_res.iter()) {
+        //     if r1 != r2 {
+        //         println!("my_res = {:?}", r1);
+        //         println!("true_res = {:?}", r2);
+        //     }
+        // }
+        assert_eq!(my_res, true_res);
     }
 }
