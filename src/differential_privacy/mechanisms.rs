03f2ab7cdb4f7ab2fb5a4f47ced5512378633498
victoria de sainte agathe <vdsa@sarus.tech>
2023-07-13 16:51:53 +0200

    add precision

--- src/differential_privacy/mechanisms.rs
+++ src/differential_privacy/mechanisms.rs
@@ -1,4 +1,8 @@
-use statrs::distribution::{ContinuousCDF, Normal};
+use statrs::{
+    prec::F64_PREC,
+    distribution::{ContinuousCDF, Normal}
+};
+
 
 pub fn gaussian_noise(epsilon: f64, delta: f64, sensitivity: f64) -> f64 {
     (2. * (1.25_f64.ln() / delta)).sqrt() * sensitivity / epsilon
@@ -7,5 +11,5 @@ pub fn gaussian_noise(epsilon: f64, delta: f64, sensitivity: f64) -> f64 {
 pub fn gaussian_tau(epsilon: f64, delta: f64, sensitivity: f64) -> f64 {
     let dist = Normal::new(0.0, 1.0).unwrap();
     let scale = gaussian_noise(epsilon, delta, sensitivity);
-    1. + scale * dist.inverse_cdf((1. - delta / 2.).powf(1. / sensitivity))
+    1. + scale * dist.inverse_cdf((1. - delta / 2.).powf(1. / sensitivity)) + F64_PREC
 }
