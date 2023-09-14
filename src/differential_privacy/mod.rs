46d94bfc11e9bd0416957c55d7f107a9674cfce3
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-09-14 19:13:59 +0200

    OK

--- src/differential_privacy/mod.rs
+++ src/differential_privacy/mod.rs
@@ -57,6 +57,25 @@ impl From<transforms::Error> for Error {
 impl error::Error for Error {}
 pub type Result<T> = result::Result<T, Error>;
 
+/// A DP Relation
+#[derive(Clone, Debug)]
+pub struct DPRelation(pub Relation);
+
+impl From<DPRelation> for Relation {
+    fn from(value: DPRelation) -> Self {
+        value.0
+    }
+}
+
+impl Deref for DPRelation {
+    type Target = Relation;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
 impl Field {
     pub fn clipping_value(self, multiplicity: i64) -> f64 {
         match self.data_type() {
@@ -76,25 +95,28 @@ impl Field {
 }
 
 impl PEPRelation {
-    /// Compile a protected Relation into DP
-    pub fn dp_compile_counts_and_sums(self, epsilon: f64, delta: f64) -> Result<Relation> {// Return a DP relation
-        let protected_entity_id = self.protected_entity_id().to_string();
-        let protected_entity_weight = self.protected_entity_weight().to_string();
-        if let PEPRelation(Relation::Reduce(reduce)) = self {
-            reduce.dp_compile_counts_and_sums(&protected_entity_id, &protected_entity_weight, epsilon, delta)
-        } else {
-            Err(Error::invalid_relation(self.0))
-        }
-    }
+    // /// Compile a protected Relation into DP
+    // pub fn dp_compile_sums(self, epsilon: f64, delta: f64) -> Result<DPRelation> {// Return a DP relation
+    //     let protected_entity_id = self.protected_entity_id().to_string();
+    //     let protected_entity_weight = self.protected_entity_weight().to_string();
+    //     if let PEPRelation(Relation::Reduce(reduce)) = self {
+    //         reduce.dp_compile_sums(&protected_entity_id, &protected_entity_weight, epsilon, delta)
+    //     } else {
+    //         Err(Error::invalid_relation(self.0))
+    //     }
+    // }
 
     /// Compile a protected Relation into DP
-    pub fn dp_compile(self, epsilon: f64, delta: f64) -> Result<Relation> {// Return a DP relation
+    pub fn dp_compile(self, epsilon: f64, delta: f64) -> Result<DPRelation> {// Return a DP relation
         let protected_entity_id = self.protected_entity_id().to_string();
         let protected_entity_weight = self.protected_entity_weight().to_string();
-        if let PEPRelation(Relation::Reduce(reduce)) = self {
-            reduce.dp_compile(&protected_entity_id, &protected_entity_weight, epsilon, delta)
-        } else {
-            Err(Error::invalid_relation(self.0))
+        match Relation::from(self) {
+            Relation::Map(map) => {
+                let dp_input = PEPRelation(map.input().clone()).dp_compile(epsilon, delta)?;
+                Ok(DPRelation(Map::builder().with(map).input(Relation::from(dp_input)).build()))
+            },
+            Relation::Reduce(reduce) => reduce.dp_compile_sums(&protected_entity_id, &protected_entity_weight, epsilon, delta),
+            relation => Err(Error::invalid_relation(relation))
         }
     }
 }
@@ -103,7 +125,7 @@ impl PEPRelation {
  */
 impl Reduce {
     /// DP compile the sums
-    fn dp_compile_counts_and_sums(self, protected_entity_id: &str, protected_entity_weight: &str, epsilon: f64, delta: f64) -> Result<Relation> {
+    fn dp_compile_sums(self, protected_entity_id: &str, protected_entity_weight: &str, epsilon: f64, delta: f64) -> Result<DPRelation> {
         // Collect groups
         let mut input_entities: Option<&str> = None;
         let mut input_groups: HashSet<&str> = self.group_by_names().into_iter().collect();
@@ -123,6 +145,19 @@ impl Reduce {
                 input_values_bound.push((input_name, absolute_bound));
             }
         };
+        // Check that groups are public
+        if !input_groups.iter().all(|e| match self.input().schema()[*e].data_type() {// TODO improve this
+            DataType::Boolean(b) if b.all_values() => true,
+            DataType::Integer(i) if i.all_values() => true,
+            DataType::Enum(e) => true,
+            DataType::Float(f) if f.all_values() => true,
+            DataType::Text(t) if t.all_values() => true,
+            _ => false,
+        }) {
+            //return Err(Error::invalid_relation(self));
+            println!("GROUPS SHOULD BE PUBLIC")
+        };
+        // Clip the relation
         let clipped_relation = self.input().clone().l2_clipped_sums(
             input_entities.unwrap(),
             input_groups.into_iter().collect(),
@@ -131,11 +166,11 @@ impl Reduce {
         let noise_multiplier = 1.; // TODO set this properly
         let dp_clipped_relation = clipped_relation.add_gaussian_noise(input_values_bound.into_iter().map(|(name, bound)| (name,noise_multiplier*bound)).collect());
         let renamed_dp_clipped_relation = dp_clipped_relation.rename_fields(|n, e| names.get(n).unwrap_or(&n).to_string());
-        Ok(renamed_dp_clipped_relation)
+        Ok(DPRelation(renamed_dp_clipped_relation))
     }
 
     /// Rewrite aggregations as sums and compile sums
-    pub fn dp_compile(self, protected_entity_id: &str, protected_entity_weight: &str, epsilon: f64, delta: f64) -> Result<Relation> {
+    pub fn dp_compile(self, protected_entity_id: &str, protected_entity_weight: &str, epsilon: f64, delta: f64) -> Result<DPRelation> {
         let mut output = Map::builder();
         let mut sums = Reduce::builder();
         // Add aggregate colums
@@ -143,110 +178,38 @@ impl Reduce {
             match aggregate.aggregate() {
                 aggregate::Aggregate::First => {
                     sums = sums.with((aggregate.column_name()?, AggregateColumn::col(aggregate.column_name()?)));
-                    output = output.with((name, Expr::col(aggregate.column_name()?)));
                 },
                 aggregate::Aggregate::Mean => {
                     let sum_col = &format!("_SUM_{}", aggregate.column_name()?);
                     let count_col = &format!("_COUNT_{}", aggregate.column_name()?);
                     sums = sums
-                        .with((count_col, AggregateColumn::count(aggregate.column_name()?)))
-                        .with((sum_col, AggregateColumn::sum(aggregate.column_name()?)));
+                        .with((count_col, Expr::sum(Expr::val(1.))))
+                        .with((sum_col, Expr::sum(Expr::col(aggregate.column_name()?))));
                     output = output
-                        .with((name, Expr::divide(Expr::col(sum_col), Expr::col(count_col))))
+                        .with((name, Expr::divide(Expr::col(sum_col), Expr::greatest(Expr::val(1.), Expr::col(count_col)))))
                 },
                 aggregate::Aggregate::Count => {
                     let count_col = &format!("_COUNT_{}", aggregate.column_name()?);
-                    sums = sums.with((count_col, AggregateColumn::count(aggregate.column_name()?)));
+                    sums = sums.with((count_col, Expr::sum(Expr::val(1.))));
                     output = output.with((name, Expr::col(count_col)));
                 },
-                aggregate::Aggregate::Sum => {
+                aggregate::Aggregate::Sum if aggregate.column_name()? != protected_entity_weight => {
                     let sum_col = &format!("_SUM_{}", aggregate.column_name()?);
-                    sums = sums.with((sum_col, AggregateColumn::sum(aggregate.column_name()?)));
+                    sums = sums.with((sum_col, Expr::sum(Expr::col(aggregate.column_name()?))));
                     output = output.with((name, Expr::col(sum_col)));
                 },
                 aggregate::Aggregate::Std => todo!(),
                 aggregate::Aggregate::Var => todo!(),
-                _ => return Err(Error::invalid_relation(self)),
+                _ => (),
             }
         }
         sums = sums.group_by_iter(self.group_by().iter().cloned());
         let sums: Reduce = sums.input(self.input().clone()).build();
-        Relation::from(sums.clone()).display_dot().unwrap();
-        println!("DEBUG output {:#?}", output);
-        // let dp_sums = sums.dp_compile_counts_and_sums(protected_entity_id, protected_entity_weight, epsilon, delta)?;
-        // dp_sums.clone().display_dot().unwrap();
-        Ok(output.input(Relation::from(sums.clone())).build())
-    }
-
-    pub fn dp_compilation<'a>(
-        self,
-        relations: &'a Hierarchy<Rc<Relation>>,
-        protected_entity: &'a [(&'a str, &'a [(&'a str, &'a str, &'a str)], &'a str)],
-        epsilon: f64,
-        delta: f64,
-    ) -> Result<Relation> {
-        // let multiplicity = 1; // TODO
-        // let (clipping_values, name_sigmas): (Vec<(String, f64)>, Vec<(String, f64)>) = self
-        //     .schema()
-        //     .clone()
-        //     .iter()
-        //     .zip(self.aggregate().clone().into_iter())
-        //     .fold((vec![], vec![]), |(c, s), (f, x)| {
-        //         if let (name, Expr::Aggregate(agg)) = (f.name(), x) {
-        //             match agg.aggregate() {
-        //                 aggregate::Aggregate::Sum => {
-        //                     let mut c = c;
-        //                     let cvalue = self
-        //                         .input()
-        //                         .schema()
-        //                         .field(agg.argument_name().unwrap())
-        //                         .unwrap()
-        //                         .clone()
-        //                         .clipping_value(multiplicity);
-        //                     c.push((agg.argument_name().unwrap().to_string(), cvalue));
-        //                     let mut s = s;
-        //                     s.push((
-        //                         name.to_string(),
-        //                         mechanisms::gaussian_noise(epsilon, delta, cvalue),
-        //                     ));
-        //                     (c, s)
-        //                 }
-        //                 _ => (c, s),
-        //             }
-        //         } else {
-        //             (c, s)
-        //         }
-        //     });
-
-        // let clipping_values = clipping_values
-        //     .iter()
-        //     .map(|(n, v)| (n.as_str(), *v))
-        //     .collect();
-        // let clipped_relation = self.clip_aggregates(PE_ID, clipping_values)?;
-
-        // let name_sigmas = name_sigmas.iter().map(|(n, v)| (n.as_str(), *v)).collect();
-        // Ok(clipped_relation.add_gaussian_noise(name_sigmas))
-        todo!()
+        let dp_sums: Relation = sums.dp_compile_sums(protected_entity_id, protected_entity_weight, epsilon, delta)?.into();
+        Ok(DPRelation(output.input(dp_sums).build()))
     }
 }
 
-impl Relation {
-    pub fn dp_compilation<'a>(
-        self,
-        relations: &'a Hierarchy<Rc<Relation>>,
-        protected_entity: &'a [(&'a str, &'a [(&'a str, &'a str, &'a str)], &'a str)],
-        epsilon: f64,
-        delta: f64,
-    ) -> Result<Relation> {
-        let protected_relation: Relation = self.force_protect_from_field_paths(relations, protected_entity).into();
-        match protected_relation {
-            Relation::Reduce(reduce) => {
-                reduce.dp_compilation(relations, protected_entity, epsilon, delta)
-            }
-            _ => todo!(),
-        }
-    }
-}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -282,46 +245,6 @@ mod tests {
         {
             println!("Row = {row}");
         }
-    }
-
-    #[test]
-    fn test_dp_compile_sums() {
-        let mut database = postgresql::test_database();
-        let relations = database.relations();
-
-        let table = relations
-            .get(&["item_table".into()])
-            .unwrap()
-            .as_ref()
-            .clone();
-
-        // with GROUP BY
-        let relation: Relation = Relation::reduce()
-            .input(table.clone())
-            .with(("sum_price", Expr::sum(Expr::col("price"))))
-            // .with_group_by_column("order_id")
-            .group_by(Expr::col("order_id"))
-            .build();
-        relation.display_dot().unwrap();
-
-        let pep_relation = relation.force_protect_from_field_paths(&relations, &[
-            (
-                "item_table",
-                &[
-                    ("order_id", "order_table", "id"),
-                    ("user_id", "user_table", "id"),
-                ],
-                "name",
-            ),
-            ("order_table", &[("user_id", "user_table", "id")], "name"),
-            ("user_table", &[], "name"),
-        ]);
-        pep_relation.display_dot().unwrap();
-
-        let epsilon = 1.;
-        let delta = 1e-3;
-        let dp_relation = pep_relation.dp_compile_counts_and_sums(epsilon, delta).unwrap();
-        dp_relation.display_dot().unwrap();
     }
 
     #[test]
@@ -364,53 +287,5 @@ mod tests {
         let delta = 1e-3;
         let dp_relation = pep_relation.dp_compile(epsilon, delta).unwrap();
         dp_relation.display_dot().unwrap();
-    }
-
-    #[ignore]// TODO reactivate this
-    #[test]
-    fn test_dp_compilation() {
-        let mut database = postgresql::test_database();
-        let relations = database.relations();
-
-        let table = relations
-            .get(&["item_table".into()])
-            .unwrap()
-            .as_ref()
-            .clone();
-
-        // with GROUP BY
-        let relation: Relation = Relation::reduce()
-            .input(table.clone())
-            .with(("sum_price", Expr::sum(Expr::col("price"))))
-            .with_group_by_column("order_id")
-            .build();
-
-        let epsilon = 1.;
-        let delta = 1e-3;
-        let dp_relation = relation
-            .dp_compilation(
-                &relations,
-                &[
-                    (
-                        "item_table",
-                        &[
-                            ("order_id", "order_table", "id"),
-                            ("user_id", "user_table", "id"),
-                        ],
-                        "name",
-                    ),
-                    ("order_table", &[("user_id", "user_table", "id")], "name"),
-                    ("user_table", &[], "name"),
-                ],
-                epsilon,
-                delta,
-            )
-            .unwrap();
-        dp_relation.display_dot().unwrap();
-
-        let query: &str = &ast::Query::from(&dp_relation).to_string();
-        println!("Query: {}", query);
-        let my_res = database.query(query).unwrap();
-        println!("\n\nresults = {:?}", my_res);
     }
 }
