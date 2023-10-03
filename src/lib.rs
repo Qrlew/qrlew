f2efe01ca8fbbe7b6fdf4c1e367cfc55aab487db
Nicolas Grislain <nicolas.grislain@gmail.com>
2023-10-03 23:48:17 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,6 @@ pub mod setup;
 #[macro_use]
 pub mod expr;
 pub mod builder;
-pub mod compilation;
 pub mod debug;
 pub mod differential_privacy;
 pub mod display;
@@ -30,6 +29,7 @@ pub mod io;
 pub mod namer;
 pub mod protection;
 pub mod relation;
+pub mod rewriting;
 pub mod sampling_adjustment;
 pub mod sql;
 pub mod types;
